945bf0a1d3a01d674b39d8c54e922ab2fabe2109
Thiago Dutra <thiiagodutra@gmail.com>
2023-01-13 17:03:22 -0300

    documented code

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ Página HTML com modal que contém um slider de imagens pré-armazenadas
 
 O projeto utiliza Javascript no modal e nas trocas das imagens.
 Ao clicar no botão de seta para esquerda, acontece a troca para a imagem imediatamente anterior, enquanto ao clicar no botão de seta para direita, a imagem troca para a próxima.
+Ao parar com o mouse em cima da imagem, ela muda automáticamente a cada 2s.
 
 --------------------------------------------------------
 EN
 
 HTML Page with a button that opens a Modal that contains a image slider.
 When you click to left, the image change to the previous image, and when you click to right, it's change to the next image.
+When the mouse pointer hovers the image automatically it switchs to the next one in 2 seconds.
--- index.js
+++ index.js
@@ -1,29 +1,36 @@
+// IDENTIFIED THE ELEMENTS
 const modal = document.getElementById("myModal");
 const btnModal = document.getElementById("btnModal")
 const span = document.getElementsByClassName("close")[0];
 
+// ADD ONCLICK EVENT ON MODAL BUTTON
 btnModal.onclick = () => {
     modal.style.display = "block";
 }
 
+// ADD ONCLICK EVENT ON "X" OF THE MODAL
 span.onclick = () => {
     modal.style.display = "none";
 }
 
+// ADD EVENT THAT WHERE YOU CLICK, THE MODAL CLOSE
 window.onclick = (e) => {
     if(e.target == modal){
         modal.style.display = "none";
     }
 }
 
+// INITIALIZE A VARIABLE WITH THE IMAGE ELEMENTS
 let images = document.getElementsByClassName("thumbs");
 let size = images.length;
 let slideAtual = 0;
 let tmpSlider;
 
+// IDENTIFIED THE NEXT AND PREVIOUS BUTTONS
 const btnRight = document.getElementById("right");
 const btnLeft = document.getElementById("left");
 
+// FUNCTION TO SWITCH IMAGES WHEN THE MOUSE HOVERS
 function autoChange(){
     images[slideAtual].style.display = "none";
     slideAtual++;
@@ -33,14 +40,22 @@ function autoChange(){
     images[slideAtual].style.display = "block";
 }
 
+// START THE AUTOCHANGE FUNCTION ONE EVERY 2 SECONDS
 function startSlide(){
     images[slideAtual].style.display = "block";
     tmpSlider = setInterval(autoChange, 2000);
 }
 
+// FUNCTION TO STOP THE AUTOCHANGE WHEN THE MOUSE LEAVE
+function stop(){
+    clearInterval(tmpSlider);
+}
+
+// GET THE POSITION OF IMAGE AND CHANGE TO THE PREVIOUS POSITION
 function changeLeft(){
     images[slideAtual].style.display = "none";
     slideAtual--;
+    // WHEN POSITION < SIZE, IT HAVE TO RESTART TO THE LAST POSITION
     if(slideAtual < 0){
         slideAtual = size - 1;
         images[slideAtual].style.display = "block";
@@ -48,13 +63,11 @@ function changeLeft(){
     images[slideAtual].style.display = "block";
 }
 
-function stop(){
-    clearInterval(tmpSlider);
-}
-
+// GET THE POSITION OF IMAGE AND CHANGE TO THE NEXT
 function changeRight(){
     images[slideAtual].style.display = "none";
     slideAtual++;
+    // WHEN POSITION >= SIZE, IT HAVE TO RESTART
     if(slideAtual >= size){
         slideAtual = 0;
         images[slideAtual].style.display = "block";
@@ -62,6 +75,7 @@ function changeRight(){
     images[slideAtual].style.display = "block";
 }
 
-btnLeft.addEventListener("click", changeLeft)
 
+// EVENT LISTNERS
+btnLeft.addEventListener("click", changeLeft)
 btnRight.addEventListener("click", changeRight)
